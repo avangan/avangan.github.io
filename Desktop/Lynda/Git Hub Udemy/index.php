26b98957461ebafb54bb630c63eea2476db2f33d
Nick kozlakgit config --global user.email kozjohn@dunwoody.edu <kozjohn@dunwoody.edu>
2019-01-18 10:55:20 -0600

    first Type

---
+++ Desktop/Lynda/Git Hub Udemy/index.php
@@ -0,0 +1,14 @@
+<?php
+include('connect.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Simple Git Project </title>
+  <meta charset="utf-8">
+
+</head>
+<body>
+<p>Simple Git Program</p>
+</body>
+</html>
